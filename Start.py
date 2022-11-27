613612e20df369aa16d3a8d7a2b31fc736fe3d58
Nikolai Riabykh <nriabykh@gmail.com>
2022-11-28 02:48:26 +0600

    add version and custom footer

--- Start.py
+++ Start.py
@@ -1,10 +1,28 @@
 import streamlit as st
 from common.static import start_caption
+from version import __version__ as version
+
 
 st.set_page_config(
     page_title='TS Clustering',
     layout='wide'
 )
+
+custom_footer = f"""
+    <style>
+    /* This is to hide Streamlit footer */
+    footer {{visibility: hidden;}}
+    footer:after {{
+        content:'v.{version} (88milesprower. 2022)'; 
+        visibility: visible;
+        display: block;
+        position: relative;
+        padding: 5px;
+        top: 2px;
+    }}
+"""
+
+st.markdown(custom_footer, unsafe_allow_html=True)
 
 st.header("Time series clustering")
 st.caption(start_caption)
